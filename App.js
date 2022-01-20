fdc611f269ff8c687bb8437b05a44e36198341cb
Muhammad Shahzaib Ishaq <shexbi119@gmail.com>
2022-01-20 13:00:58 +0500

    started

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import Crud from "./screens/Crud";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {/* <Text>Open up App.js to start working on your app!</Text> */}
       <Crud />
       <StatusBar style="auto" />
     </View>
--- screens/Crud.js
+++ screens/Crud.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { View, Text } from "react-native";
 
 const Crud = () => {
-  <View>
-    <Text> Hello </Text>
-  </View>;
+  return (
+    <View>
+      <Text> Hello </Text>
+    </View>
+  );
 };
 
 export default Crud;
